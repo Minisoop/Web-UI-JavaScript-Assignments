81459c167b8e5ebae2c0f11be115f9053fc7d3a9
Ben Cooper <bencooper5@hotmail.com>
2021-11-16 17:30:02 +0000

    Altered email pattern matching

--- 4_match_email.js
+++ 4_match_email.js
@@ -1,6 +1,9 @@
 function matchEmail(emailIn){
-    let mailPattern = /[a-zA-Z0-9]/;
+    let mailPattern = /[a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+(\.[a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+)*@+[a-zA-Z0-9]+\.+[a-zA-Z0-9]/;
     return mailPattern.test(emailIn);
 }
 
-console.log(matchEmail("ALIAS"));
+console.log(matchEmail("memail.com"));
+console.log(matchEmail("my.mail727@gmail.com"));
+console.log(matchEmail(".memail@gmail.com"));
+console.log(matchEmail(".memail@uni.ac.uk"));
